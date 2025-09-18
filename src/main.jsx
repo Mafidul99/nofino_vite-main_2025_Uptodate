3894e3c4ae0b489ce67420d49d75622ff6587157
Mafidul99 <mdonline192@gmail.com>
2025-09-18 15:36:01 +0530

    manim up

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import '@fontsource/roboto';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <ToastContainer 
+      position="top-right"
+      toastClassName="toastbody"
+      theme="colored"
+      pauseOnFocusLoss={false}
+      pauseOnHover={true}
+      draggable={true}
+      autoClose={3000}
+      draggablePercent={60}
+      closeOnClick={true}
+    />
   </StrictMode>,
 )
