ada52565061ed040e2e6f7f30f6acc36a2d25e02
Mafidul99 <mdonline192@gmail.com>
2025-09-20 12:04:08 +0530

    applyout

--- src/components/Layouts/AppLayout.jsx
+++ src/components/Layouts/AppLayout.jsx
@@ -1,14 +1,14 @@
-import { Outlet  } from "react-router-dom";
+import { Outlet, useNavigation  } from "react-router-dom";
 import Headers from "../UI/Headers";
 import Footers from "../UI/Footers";
 import ScrollTop from "./ScrollTop";
-// import NaviLoader from "./NaviLoader";
+import NaviLoader from "./NaviLoader";
 
 export const AppLayout = () => {
 
-    // const navgation = useNavigation();
-    // if (navgation.state === "loading")
-    //     return <NaviLoader />;
+    const navgation = useNavigation();
+    if (navgation.state === "loading")
+        return <NaviLoader />;
 
 
     return (
